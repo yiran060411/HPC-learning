c043463d6abcfb9bc023098b55eb083031585089
yiran060411 <nyr060411@qq.com>
2025-05-22 17:58:03 +0800

    Create do1.f90

---
+++ 5.22/code/do1.f90
@@ -0,0 +1,11 @@
+program printNum 
+implicit none  
+   ! define variables
+   integer :: n
+   
+   do n = 11, 20     
+      ! printing the value of n 
+      print*,  n 
+   end do 
+   
+end program printNum  
