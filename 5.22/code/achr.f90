1d2a29ecb2bcc013b5ba57656ea4811f80b3e1da
yiran060411 <nyr060411@qq.com>
2025-05-22 17:55:04 +0800

    Create achr.f90

---
+++ 5.22/code/achr.f90
@@ -0,0 +1,11 @@
+program testingChars
+implicit none
+   character:: ch
+   integer:: i
+   
+   do i = 65, 90
+      ch = achar(i)
+      print*, i, ' ', ch
+   end do
+   
+end program testingChars
