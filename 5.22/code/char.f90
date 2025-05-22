d2bbd964634c3e121286264a454700313c643add
yiran060411 <nyr060411@qq.com>
2025-05-22 17:55:42 +0800

    Create char.f90

---
+++ 5.22/code/char.f90
@@ -0,0 +1,15 @@
+program hello
+implicit none
+   character(len = 15) :: surname, firstname 
+   character(len = 6) :: title 
+   character(len = 25)::greetings
+   
+   title = 'Mr. ' 
+   firstname = 'Rowan ' 
+   surname = 'Atkinson'
+   greetings = 'A big hello from Mr. Bean'
+   
+   print *, 'Here is ', title, firstname, surname
+   print *, greetings
+   
+end program hello
