75cf28d71ececd14bab023d566923637d1b15151
yiran060411 <nyr060411@qq.com>
2025-05-23 21:35:29 +0800

    Create adjust.f90

---
+++ 5.23/code/adjust.f90
@@ -0,0 +1,25 @@
+program hello
+implicit none
+   character(len = 15) :: surname, firstname 
+   character(len = 6) :: title 
+   character(len = 40):: name
+   character(len = 25):: greetings
+   
+   title = 'Mr. ' 
+   firstname = 'Rowan' 
+   surname = 'Atkinson'
+   greetings = 'A big hello from Mr. Beans'
+   
+   name = adjustl(title)//adjustl(firstname)//adjustl(surname)
+   print *, 'Here is', name
+   print *, greetings
+   
+   name = adjustr(title)//adjustr(firstname)//adjustr(surname)
+   print *, 'Here is', name
+   print *, greetings
+   
+   name = trim(title)//trim(firstname)//trim(surname)
+   print *, 'Here is', name
+   print *, greetings
+   
+end program hello
