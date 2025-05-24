ef7feec09ce661ceef19ee883d512f1be6cc9897
yiran060411 <nyr060411@qq.com>
2025-05-24 21:57:42 +0800

    Create 定位函数.f90

---
+++ 5.24/code/定位函数.f90
@@ -0,0 +1,7 @@
+program arrayLocation
+implicit none
+   real, dimension(1:6) :: a = (/ 21.0, 12.0,33.0, 24.0, 15.0, 16.0 /)
+   Print *, maxloc(a)
+   Print *, minloc(a)
+   
+end program arrayLocation   
